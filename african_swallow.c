6956b28e219db03f48a7fa8af13c9ada8f1306dd
Travis <6611@holbertonstudents.com>
2023-08-09 15:27:15 -0700

    Done fixing print stderr issues

--- african_swallow.c
+++ african_swallow.c
@@ -11,7 +11,7 @@ void file_exists(char *path_name, char *file_name)
 {
 	if (access(path_name, F_OK) == -1  || access(path_name, R_OK) == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", file_name);
+		fprintf(stderr, "Error: Can't open file %s\n", file_name);
 		free(path_name);
 		exit(EXIT_FAILURE);
 	}
--- m_funcs.c
+++ m_funcs.c
@@ -14,7 +14,7 @@ void push_m(stack_t **stack, unsigned int line_number)
 
 	if (!new_line[1] || !strcmp(new_line[1], ""))
 	{
-		fprintf(stderr, "l%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		free(new_line[0]);
 		exit(EXIT_FAILURE);
 	}
