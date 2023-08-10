f7f4dc5df7bb528479a5e03eb7a71a98f3799e48
Travis <6611@holbertonstudents.com>
2023-08-10 09:06:56 -0700

    Added swap

---
+++ m_funcs_more.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * swap_m - Swaps the top two elements
+ * @stack: The stack to swap in
+ * @line_number: The existing line number
+ *
+ * Return: No return value
+ */
+void swap_m(stack_t **stack, unsigned int line_number)
+{
+	stack_t *swap_stack;
+
+	if (!*stack || !*stack->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	swap_stack = *stack;
+	swap_stack->prev = swap_stack->next;
+	swap_stack->next = swap_stack->prev->next;
+	swap_stack->prev->prev = NULL;
+	swap_stack->prev->next = swap_stack;
+
+	*stack = swap_stack->prev;
+}
